db5d37ab2b69f54617b9b321e7015497abbca07a
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-02-20 13:15:48 +0100

    models

--- models/User.model.js
+++ models/User.model.js
@@ -1,26 +1,25 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: false,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+
+const userSchema = new Schema ({
+  name: { 
+      type: String, 
+      required: true 
   },
+  password: {
+      type: String,
+      required: true
+  },
+  city: {
+      type: String,
+      required: true
+  },
+  image: String,
+  info: String,
+  pinPersonal: [ { type: Schema.Types.ObjectId, ref: "Post" } ],
+  pinFav: [ { type: Schema.Types.ObjectId, ref: "Post" } ],
+  admin: Boolean,
+},
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
