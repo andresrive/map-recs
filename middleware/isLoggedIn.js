86429989d109c6d0248834d7fa1d2307874c20d5
Beartoe7 <64451243+Beartoe7@users.noreply.github.com>
2023-02-21 10:43:53 +0100

    Roberto master

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,8 +1,4 @@
 module.exports = (req, res, next) => {
-  // checks if the user is logged in when trying to access a specific page
-  if (!req.session.currentUser) {
-    return res.redirect("/auth/login");
-  }
-
-  next();
-};
+  if(req.session.currentUser) next();
+  else res.redirect("/auth/login");
+}
